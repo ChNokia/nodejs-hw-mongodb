034276ac3b9fe464a924c5042ea25e67c26dd00f
Sergiy Chyruk <sergii.chyruk@dxc.com>
2025-03-28 16:05:19 +0200

    node-hw2

---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import { getEnvVar } from '../utils/getEnvVar.js';
+import { getContactById, getContacts } from '../services/contacts.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    const data = await getContacts();
+    res.json({ status: 200, message: 'Successfully found contacts!', data });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const data = await getContactById(contactId);
+
+    if (!data) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data,
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
